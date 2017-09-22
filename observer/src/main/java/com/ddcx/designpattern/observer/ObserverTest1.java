afd124f3cda08e9888e2d361558c63b3c97bb737
liaosi <liaosilzu2007@163.com>
2017-09-22 15:11:36 +0800

    # 观察者模式1

---
+++ observer/src/main/java/com/ddcx/designpattern/observer/ObserverTest1.java
@@ -0,0 +1,78 @@
+package com.ddcx.designpattern.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by liaosi on 2017/9/22.
+ */
+public class ObserverTest1 {
+
+    public static void main(String[] args) {
+        Transporter transporter = new Transporter();
+        transporter.addObserver(new Guard());
+        transporter.addObserver(new Police());
+        transporter.notifyObserver();
+    }
+
+}
+
+/**
+ * 抽象的观察者
+ */
+interface Observer {
+    public void update();
+}
+
+class Guard implements Observer {
+
+    @Override
+    public void update() {
+        System.out.println("保镖开始进行保卫工作...");
+    }
+}
+
+class Police implements Observer {
+
+    @Override
+    public void update() {
+        System.out.println("警察开始出动进行保卫工作...");
+    }
+}
+
+interface Subject {
+
+    public void addObserver(Observer observer);
+    public void removeObserver(Observer observer);
+    public void notifyObserver();
+
+}
+
+class Transporter implements Subject {
+
+    private List<Observer> observerList;
+
+    public Transporter() {
+        this.observerList = new ArrayList<>();
+    }
+
+    @Override
+    public void addObserver(Observer observer) {
+        observerList.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        int i = observerList.indexOf(observer);
+        if (i > 0) {
+            observerList.remove(observer);
+        }
+    }
+
+    @Override
+    public void notifyObserver() {
+        for (Observer observer : observerList) {
+            observer.update();
+        }
+    }
+}
